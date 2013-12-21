ff8189707b5ab2a58ef0c7ee565408083457fa41
Ayax50000 <fjlsoto@gmail.com>
2013-12-21 17:47:39 -0600

    comenzando a programar el analisis de expresion

--- format.rb
+++ format.rb
@@ -1,7 +1,11 @@
 class Format
     def initialize
     end
-    def self.check
+    def self.check(expresion)
+        hash = { 1 => ["<","(","{","["] , 2 => [">",")","}","]" ] , 3 => false , 4 => [0,0,0,0] }
+        expresion = epresion.to_s
+        expresion.each_char do |ent|
 
+        end 
     end
 end
