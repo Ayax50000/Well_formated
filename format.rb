c76f4b16de7456915b0eade4deff1c9101935b07
Ayax50000 <fjlsoto@gmail.com>
2013-12-25 15:41:43 -0600

    method initialize removed

--- format.rb
+++ format.rb
@@ -1,6 +1,4 @@
 class Format
-   def initialize
-    end
     def self.check(expresion)
         hash = { 1 => ["<","(","{","["] , 2 => [">",")","}","]" ] , 3 => false , 4 => [0,0,0,0] }
         expresion = expresion
