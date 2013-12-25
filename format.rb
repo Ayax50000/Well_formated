d74035294154d60e164d248d678f1821f41db66d
Ayax50000 <fjlsoto@gmail.com>
2013-12-25 16:45:18 -0600

    bug related to closing order fixed

--- format.rb
+++ format.rb
@@ -1,20 +1,20 @@
 class Format
     def self.check(expresion)
-        hash = { 1 => ["<","(","{","["] , 2 => [">",")","}","]" ] , :respuesta => false , 4 => [0,0,0,0] }
-        expresion = expresion
+        correcto , incorrecto = "correctamente formateado","incorrectamente formateado"
+        caracteres, respuesta , encontrar = { :open => ["<","(","{","["] , :close => [">",")","}","]" ] }, correcto , [""]
         expresion.each_char do |caracter|
             [0,1,2,3].each do |index|
-                hash[4][index] += 1 if hash[1].at(index) == caracter
-                if hash[2].at(index) == caracter then
-                    if hash[4].at(index) == 0 then
-                        hash[:respuesta] = true
-                    end
-                        hash[4][index] -= 1
-                end
+               encontrar << caracteres[:close][index] if caracteres[:open][index] == caracter
+               if caracteres[:close][index] == caracter then
+                  if caracter == encontrar.last then
+                    encontrar.pop
+                  else
+                    respuesta = incorrecto
+                  end
+               end
             end
         end
-        hash[:respuesta] = true unless hash[4][0..3] == [0,0,0,0]
-        puts "bien formateado" if hash[:respuesta] == false
-        puts "mal formateado" unless hash[:respuesta] == false
+        respuesta = incorrecto if encontrar != [""]
+        puts respuesta
     end
 end
