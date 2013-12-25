db413655b28484937a477ea397fb112fb5bcbc0f
Ayax50000 <fjlsoto@gmail.com>
2013-12-25 15:40:08 -0600

    fixed first comment about case a string to string

--- format.rb
+++ format.rb
@@ -1,11 +1,11 @@
 class Format
-    def initialize
+   def initialize
     end
     def self.check(expresion)
         hash = { 1 => ["<","(","{","["] , 2 => [">",")","}","]" ] , 3 => false , 4 => [0,0,0,0] }
-        expresion = expresion.to_s
+        expresion = expresion
         expresion.each_char do |ent|
-            [0,1,2,3].each do |x|   
+            [0,1,2,3].each do |x|
                 hash[4][x] += 1 if hash[1].at(x) == ent
                 if hash[2].at(x) == ent then
                     if hash[4].at(x) == 0 then
@@ -14,7 +14,7 @@ class Format
                         hash[4][x] -= 1
                 end
             end
-        end 
+        end
         hash[3] = true unless hash[4][0..3] == [0,0,0,0]
         puts "bien formateado" if hash[3] == false
         puts "mal formateado" unless hash[3] == false
